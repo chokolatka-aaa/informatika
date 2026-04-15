671e563d4617312ff8b2df106e6759a97edf668d
chokolatka-aaa <volkov.home39@gmail.com>
2026-04-15 12:56:29 +0300

    алгоритм вирішення математичних прикладів.

---
+++ matem.py
@@ -0,0 +1,11 @@
+a = int(2)
+b = int(3)
+c = int(4)
+
+x = a/2 + b**3 - 10*c
+y = (a + b*c) / (c + a**2 - 4*b) + a*b - 3*c
+z = a*(b - c) + 3*b*(a - c) - 2*c*(a + b + c**4)
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
